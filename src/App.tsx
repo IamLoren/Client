d51dfe03c1b491660f9cc8f3b6b5d8d23355e7af
Iryna Kostromytska <Iryna.Kostromytska@dataart.com>
2024-10-04 14:22:12 +0300

    added suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import { lazy } from 'react';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { lazy, Suspense } from "react";
 
-const LazyHome = lazy(()=>import('./pages/HomePage'));
-const LazyHistory = lazy(()=>import('./pages/HistoryPage'));
-const LazyAdmin = lazy(()=>import('./pages/AdminPage'));
-const LazyFavourites = lazy(()=>import('./pages/FavouritesPage'));
-const LazyError = lazy(()=>import('./pages/ErrorPage'));
+const LazyHome = lazy(() => import("./pages/HomePage"));
+const LazyHistory = lazy(() => import("./pages/HistoryPage"));
+const LazyAdmin = lazy(() => import("./pages/AdminPage"));
+const LazyFavourites = lazy(() => import("./pages/FavouritesPage"));
+const LazyError = lazy(() => import("./pages/ErrorPage"));
 
-const App:React.FC = () => {
-
+const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<LazyHome/>}/>
-      <Route path="/history" element={<LazyHistory/>}/>
-      <Route path="/favourites" element={<LazyFavourites/>}/>
-      <Route path="/admin" element={<LazyAdmin/>}/>
-      <Route path="*" element={<LazyError/>}/>
-    </Routes>
-  )
-}
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<LazyHome />} />
+        <Route path="/history" element={<LazyHistory />} />
+        <Route path="/favourites" element={<LazyFavourites />} />
+        <Route path="/admin" element={<LazyAdmin />} />
+        <Route path="*" element={<LazyError />} />
+      </Routes>
+    </Suspense>
+  );
+};
 
-export default App
+export default App;
