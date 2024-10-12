163a33bee4160dffdeddb48e7fc6099b2b0bc6c0
Iryna Kostromytska <Iryna.Kostromytska@dataart.com>
2024-10-12 14:19:31 +0300

    feat: added form validation

--- src/components/SignInForm/SignInForm.tsx
+++ src/components/SignInForm/SignInForm.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { useFormik } from "formik";
+import * as Yup from 'yup';
 import Button from '../Button/Button';
 
 const SignInForm: React.FC = () => {
+
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email('Invalid email address')
+      .required('Required'),
+    password: Yup.string()
+      .min(8, 'Must be at least 8 characters')
+      .required('Required'),
+  });
+
   const formik = useFormik({
     initialValues: {
       email: "",
       password: "",
     },
+    validationSchema,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
@@ -23,6 +35,9 @@ const SignInForm: React.FC = () => {
       onChange={formik.handleChange}
       value={formik.values.email}
     />
+    {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
 
     <label htmlFor="password">Password</label>
     <input
@@ -32,6 +47,9 @@ const SignInForm: React.FC = () => {
       onChange={formik.handleChange}
       value={formik.values.password}
     />
+    {formik.touched.password && formik.errors.password ? (
+        <div>{formik.errors.password}</div>
+      ) : null}
 
     <Button type="submit"  buttonName="Submit"></Button>
   </form>
--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 import { useFormik } from "formik";
+import * as Yup from 'yup';
 import Button from "../Button/Button";
 
 const SignUpForm: React.FC = () => {
+
+  const validationSchema = Yup.object({
+    firstName: Yup.string()
+    .min(2, 'Must be at least 2 characters')
+    .max(20, 'Must be 20 characters or less')
+    .required('Required'),
+    lastName: Yup.string()
+    .min(2, 'Must be at least 2 characters')
+    .max(20, 'Must be 20 characters or less')
+    .required('Required'),
+    email: Yup.string()
+      .email('Invalid email address')
+      .required('Required'),
+    password: Yup.string()
+      .min(8, 'Must be at least 8 characters')
+      .required('Required'),
+  });
+
   const formik = useFormik({
     initialValues: {
       firstName: "",
@@ -10,13 +29,14 @@ const SignUpForm: React.FC = () => {
       email: "",
       password: "",
     },
+    validationSchema,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
   });
 
   return (
-    <form onSubmit={formik.handleSubmit}>
+    <form onSubmit={formik.handleSubmit} >
       <label htmlFor="firstName">First Name</label>
       <input
         id="firstName"
@@ -25,6 +45,9 @@ const SignUpForm: React.FC = () => {
         onChange={formik.handleChange}
         value={formik.values.firstName}
       />
+       {formik.touched.firstName && formik.errors.firstName ? (
+        <div>{formik.errors.firstName}</div>
+      ) : null}
 
       <label htmlFor="lastName">Last Name</label>
       <input
@@ -34,6 +57,9 @@ const SignUpForm: React.FC = () => {
         onChange={formik.handleChange}
         value={formik.values.lastName}
       />
+      {formik.touched.lastName && formik.errors.lastName ? (
+        <div>{formik.errors.lastName}</div>
+      ) : null}
 
       <label htmlFor="email">Email Address</label>
       <input
@@ -43,6 +69,9 @@ const SignUpForm: React.FC = () => {
         onChange={formik.handleChange}
         value={formik.values.email}
       />
+      {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
 
       <label htmlFor="password">Password</label>
       <input
@@ -52,6 +81,9 @@ const SignUpForm: React.FC = () => {
         onChange={formik.handleChange}
         value={formik.values.password}
       />
+       {formik.touched.password && formik.errors.password ? (
+        <div>{formik.errors.password}</div>
+      ) : null}
 
       <Button type="submit" buttonName="Submit"></Button>
     </form>
