651205f8100d1e04f79a1918ff1cae5d007598a6
Iryna Kostromytska <Iryna.Kostromytska@dataart.com>
2024-10-15 10:12:42 +0300

    feat: added styles to catalog

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Container:React.FC<{children:React.ReactNode}> = ({ children }) => {
+const Container:React.FC<{addStyles: string, children:React.ReactNode}> = ({addStyles, children }) => {
     return (
-      <div className="max-w-screen-xxl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className={`max-w-screen-xxl mx-auto px-4 sm:px-6 lg:px-8 ${addStyles}`}>
         {children}
       </div>
     )
