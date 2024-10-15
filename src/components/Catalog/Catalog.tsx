e68e1c19bcd1dea56f42bd4e619548d10045b885
Iryna Kostromytska <Iryna.Kostromytska@dataart.com>
2024-10-15 10:47:00 +0300

    fix: fix button

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import CardsList from '../CardList/CardsList'
 import DateTime from '../DateTime/DateTime'
 import Button from '../Button/Button'
+import { HiArrowsUpDown } from "react-icons/hi2";
 
 const Catalog:React.FC = () => {
   return (
     <div className='flex-grow'>
         <div className='flex w-[100%] justify-between mb-[30px]'>
             <DateTime />
-            <Button type="button" buttonName="" />
+            <Button type="button" buttonName=""><HiArrowsUpDown color="white"/></Button>
             <DateTime />
         </div>
         <CardsList/>
