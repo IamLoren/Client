d57276e460ad46beb19d528b3bce229a075e59a7
Iryna Kostromytska <Iryna.Kostromytska@dataart.com>
2024-10-15 10:14:45 +0300

    fix: fixed prop types

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Container:React.FC<{addStyles: string, children:React.ReactNode}> = ({addStyles, children }) => {
+const Container:React.FC<{addStyles?: string, children:React.ReactNode}> = ({addStyles, children }) => {
     return (
       <div className={`max-w-screen-xxl mx-auto px-4 sm:px-6 lg:px-8 ${addStyles}`}>
         {children}
