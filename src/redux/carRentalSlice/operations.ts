815cb7c65982352e9be4085d563a5237a56fd9e4
Iryna Kostromytska <Iryna.Kostromytska@dataart.com>
2024-10-20 15:01:45 +0300

    fix: fixed minPrice and maxPrice in  Price slider

--- src/redux/carRentalSlice/operations.ts
+++ src/redux/carRentalSlice/operations.ts
@@ -10,11 +10,25 @@ CarInterface[],
   {
     state: RootState;
     rejectValue: string;
+    getState: ()=> void
   }
 >("cars/getAll", async (_, thunkApi) => {
+  const state = thunkApi.getState(); 
+    const role = state.auth.user.role;
   try {
     const { data } = await api.get("api/cars");
-    return data;
+
+    let prices;
+    if (role === "user") {
+      prices = data.filter((car:CarInterface) => !car.isRemoved).map((car:CarInterface) => car.price.day);
+    } else {
+      prices = data.map((car:CarInterface) => car.price.day);
+    }
+
+    const minPrice = Math.min(...prices);
+    const maxPrice = Math.max(...prices);
+
+    return {data, minPrice, maxPrice };
   } catch (error: unknown) {
     if (error instanceof Error) {
       toast.error(error.message);
