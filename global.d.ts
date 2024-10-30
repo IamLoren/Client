4f236f855b109d3a460abcb297c67a305447f215
Iryna Kostromytska <Iryna.Kostromytska@dataart.com>
2024-10-30 09:39:15 +0200

    fix: added type to window

--- global.d.ts
+++
@@ -1,9 +0,0 @@
-import { Store } from 'redux';
-import { RootState } from './src/redux/store'; 
-
-declare global {
-  interface Window {
-    store: Store<RootState>;
-  }
-}
-export {};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { createRoot } from "react-dom/client";
+import { Store } from 'redux';
+import { RootState } from './redux/store'; 
 import App from "./App.tsx";
 import "./css/index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
-
+interface MyWindow extends Window {
+  store: Store<RootState>;
+}
+declare const window: MyWindow;
 window.store=store;
 
 createRoot(document.getElementById("root")!).render(
