18a2ce09486e66b472b4b7beb02ff5da17062ae3
Iryna Kostromytska <Iryna.Kostromytska@dataart.com>
2024-11-13 15:28:39 +0200

    fix: fixed navigation after logout

--- src/components/LogoutForm/LogoutForm.tsx
+++ src/components/LogoutForm/LogoutForm.tsx
@@ -1,23 +1,33 @@
-import React from 'react'
-import Button from '../Button/Button'
-import { useAppDispatch } from '../../hooks'
-import { logoutThunk } from '../../redux/authSlice/operations'
-import { closeModal } from '../../redux/modalSlice/modalSlice'
+import React from "react";
+import Button from "../Button/Button";
+import { useAppDispatch } from "../../hooks";
+import { logoutThunk } from "../../redux/authSlice/operations";
+import { closeModal } from "../../redux/modalSlice/modalSlice";
+import { useNavigate } from "react-router-dom";
 
-const LogoutForm:React.FC = () => {
-  const dispatch = useAppDispatch()
+const LogoutForm: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-const onClickLogout = ()=> {
-  dispatch(logoutThunk());
-  dispatch(closeModal())
-}
+  const onClickLogout = () => {
+    dispatch(logoutThunk());
+    dispatch(closeModal());
+    navigate("/");
+  };
 
   return (
     <div data-cy="exit accepting">
-      <p className="mt-[10px] mb-[20px]">Do you really want to close your profile?</p>
-      <Button type="button" buttonName='Logout' style="block ml-auto" onClick={onClickLogout}></Button>
+      <p className="mt-[10px] mb-[20px]">
+        Do you really want to close your profile?
+      </p>
+      <Button
+        type="button"
+        buttonName="Logout"
+        style="block ml-auto"
+        onClick={onClickLogout}
+      ></Button>
     </div>
-  )
-}
+  );
+};
 
-export default LogoutForm
+export default LogoutForm;
